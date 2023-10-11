b0f4e24526f8c2ae179c41bbbb85c50ae295bc4f
AADITYA PRASAD <122255434+onenonlybotz@users.noreply.github.com>
2023-10-11 20:38:15 +0530

    Update __init__.py

--- Yae/__init__.py
+++ Yae/__init__.py
@@ -101,7 +101,7 @@ else:
     PORT = Config.PORT
     URL = Config.URL
 
-REDIS_URL = "redis://:V6OvHLvjEnbLzo3VKY7l1TmvA39q0zn2@redis-11612.c240.us-east-1-3.ec2.cloud.redislabs.com:11612"
+REDIS_URL = "redis://:BSbBeaVqw1VYAHIlinXjPnrKob27T6WE@redis-18773.c301.ap-south-1-1.ec2.cloud.redislabs.com:18773"
 
 
 REDIS = StrictRedis.from_url(REDIS_URL, decode_responses=True)
